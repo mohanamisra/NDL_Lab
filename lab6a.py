3244dcb2fd23f3bf7213a4f532c570a4b628206c
Mohana Misra <iammohanamisra@gmail.com>
2025-04-11 23:16:16 +0530

    Commented out optional code in lab 6a

--- lab6a.py
+++ lab6a.py
@@ -16,18 +16,20 @@ weights = som.get_weights()
 plt.imshow(weights)
 plt.colorbar()
 plt.title("Self-Organizing Map")
-
-mapped = np.array([som.winner(d) for d in data])
-
-plt.figure(figsize=(8, 8))
-plt.pcolor(weights, cmap='bone_r')
-plt.colorbar()
-for i, m in enumerate(mapped):
-    plt.plot(m[0]+0.5, m[1]+0.5, 'o', markerfacecolor='None',
-             markeredgecolor='red', markersize=12, markeredgewidth=2)
-plt.title('Data points mapped to SOM')
 plt.show()
 
-print(f"SOM training complete. Map size: {som_size}x{som_size}")
-print(f"Number of data points: {len(data)}")
-print(f"Input dimension: {data.shape[1]}")
+# OPTIONAL BELOW
+# mapped = np.array([som.winner(d) for d in data])
+#
+# plt.figure(figsize=(8, 8))
+# plt.pcolor(weights, cmap='bone_r')
+# plt.colorbar()
+# for i, m in enumerate(mapped):
+#     plt.plot(m[0]+0.5, m[1]+0.5, 'o', markerfacecolor='None',
+#              markeredgecolor='red', markersize=12, markeredgewidth=2)
+# plt.title('Data points mapped to SOM')
+# plt.show()
+#
+# print(f"SOM training complete. Map size: {som_size}x{som_size}")
+# print(f"Number of data points: {len(data)}")
+# print(f"Input dimension: {data.shape[1]}")
