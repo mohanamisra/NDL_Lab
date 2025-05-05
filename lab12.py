dac145b4ee81c54e1882e7f0704eef4aa545bdf2
Mohana Misra <iammohanamisra@gmail.com>
2025-05-05 13:11:06 +0530

    Temporary replacement with shorter code (not debugged)

--- lab12.py
+++ lab12.py
@@ -1,43 +1,25 @@
-import numpy as np
+import tensorflow as tf
 
-def softmax(x): return np.exp(x) / np.sum(np.exp(x), axis=0)
-def sigmoid(x): return 1 / (1 + np.exp(-x))
-def tanh(x): return np.tanh(x)
+# Example input: (seq_len, batch_size, input_size)
+x = tf.random.normal((5, 3, 10))  # 5 time steps, 3 batches, 10 input features
 
-def rnn_cell(xt, a_prev, p):
-    a = tanh(p["Wax"] @ xt + p["Waa"] @ a_prev + p["ba"])
-    y = softmax(p["Wya"] @ a + p["by"])
-    return a, y
+# RNN Cell
+rnn_cell = tf.keras.layers.SimpleRNNCell(20)
+h_t = [tf.zeros((3, 20))]  # initial hidden state
 
-def rnn_forward(x, a0, p):
-    a, y, a_next = [], [], a0
-    for t in range(x.shape[2]):
-        a_next, yt = rnn_cell(x[:,:,t], a_next, p)
-        a.append(a_next); y.append(yt)
-    return np.stack(a, 2), np.stack(y, 2)
+# Single RNN cell forward step
+_, h_t[0] = rnn_cell(x[0], h_t)
 
-def lstm_cell(xt, a_prev, c_prev, p):
-    z = np.vstack((a_prev, xt))
-    ft, it, ot = map(lambda k: sigmoid(p[k] @ z + p['b'+k[1]]), ['Wf','Wi','Wo'])
-    cct = tanh(p['Wc'] @ z + p['bc'])
-    c = ft * c_prev + it * cct
-    a = ot * tanh(c)
-    return a, c
+# RNN forward propagation (manually through sequence)
+h_seq = []
+for t in range(x.shape[0]):
+    _, h_t[0] = rnn_cell(x[t], h_t)
+    h_seq.append(h_t[0])
+h_seq = tf.stack(h_seq)
 
-# Minimal test
-n_x, n_a, n_y, m, T = 3, 5, 2, 4, 6
-x = np.random.randn(n_x, m, T)
-a0, c0 = np.zeros((n_a, m)), np.zeros((n_a, m))
+# LSTM Cell
+lstm_cell = tf.keras.layers.LSTMCell(20)
+state = [tf.zeros((3, 20)), tf.zeros((3, 20))]  # (hidden, cell)
 
-rnn_p = {k: np.random.randn(*s) for k, s in {
-    "Wax": (n_a,n_x), "Waa": (n_a,n_a), "Wya": (n_y,n_a),
-    "ba": (n_a,1), "by": (n_y,1)}.items()}
-
-lstm_p = {k: np.random.randn(n_a, n_a+n_x) for k in ['Wf','Wi','Wo','Wc']}
-lstm_p.update({f"b{k[1]}": np.random.randn(n_a,1) for k in lstm_p})
-
-a_rnn, y_rnn = rnn_forward(x, a0, rnn_p)
-a_lstm, _ = lstm_cell(x[:,:,0], a0, c0, lstm_p)
-
-print("RNN y[0,:,0]:", y_rnn[0,:,0])
-print("LSTM a[:,0]:", a_lstm[:,0])
+# Single LSTM cell forward step
+_, state = lstm_cell(x[0], state)
